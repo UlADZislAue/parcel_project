89c8b178b7220e0b72a44351e452a671bc7a5c43
Владислав Козел <91662982+UlADZislAue@users.noreply.github.com>
2021-11-11 03:03:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const getDay = () => {
+	 const today = new Date();
+	 const daysArr = [
+	 "Sunday",
+	 "Monday",
+	 "Tuesday",
+	 "Wednesday",
+	 "Thursday",
+	 "Friday",
+	 "Saturday",
+	 ];
+	 return daysArr[today.getDay()];
+	 };
+	
+	 const day = document.querySelector('.today');
+	 day.innerHTML = getDay();
